9bc7134eff447804afd0cb503245094f74270d1c
Claudia Ramirez <mely.ramiz07@gmail.com>
2022-06-05 02:27:22 -0500

    Feat: Add concat file

---
+++ develop/concat.js
@@ -0,0 +1,9 @@
+function contacExample(){
+    const num1 = [1, 2, 3];
+    const num2 = [4, 5, 6];
+    const num3 = [7, 8, 9];
+    
+    const numbers = num1.concat(num2, num3);
+    
+    console.log(numbers);
+    }
